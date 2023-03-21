677065e3e0798d3f6aaa83d7950d67c042623b24
Santosh Kumar <sntshkmr60@gmail.com>
2023-03-21 07:29:52 +0530

    Print hello world to Serial output

---
+++ hello-world/hello-world.ino
@@ -0,0 +1,8 @@
+void setup() {
+    Serial.begin(115200); // Set the baudrate to 115200,same as the software settings
+}
+
+void loop() {
+    Serial.println("Hello World!"); // Print character string "Hello World！" on the serial
+    delay(5000); // Delay 5 second
+}
